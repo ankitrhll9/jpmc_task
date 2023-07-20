9b102b194d25e17f36fd2a932489ff9dbba377b2
Ankit <ankitrhll9@gmail.com>
2023-07-20 15:17:56 +0530

    solved first two sub tasks

--- task.py
+++ task.py
@@ -2,10 +2,19 @@
 
 gbce = {
     #Stock Symbol, Type, Last Dividend, Fixed Dividend, Par Value
-    'TEA':['Common',0,' ',100],
-    'POP':['Common',8,' ',100],
-    'ALE':['Common',23,' ',60],
-    'GIN':['Preferred',8,2/100,100],
-    'JOE':['Common',13,' ',250],
+    "TEA":["Common",0," ",100],
+    "POP":["Common",8," ",100],
+    "ALE":["Common",23," ",60],
+    "GIN":["Preferred",8,2/100,100],
+    "JOE":["Common",13," ",250],
 }
 
+for k,v in gbce.items():  
+
+    divident_yield = v[1]/v[3]  
+    print("Dividend Yield for "+k+" is "+str(divident_yield)+"%")
+
+    try:
+        print("PE Ratio for "+k+" is "+ str(v[3]/divident_yield))
+    except ZeroDivisionError:
+        print("PE Ratio for "+k+" is "+ str(0))   
