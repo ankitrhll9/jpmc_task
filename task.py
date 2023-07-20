8f7e49068a76a1216171d1996239e0b676ccc5c5
Ankit <ankitrhll9@gmail.com>
2023-07-20 15:02:00 +0530

    added the sample data

---
+++ task.py
@@ -0,0 +1,11 @@
+#Please find the code below:
+
+gbce = {
+    #Stock Symbol, Type, Last Dividend, Fixed Dividend, Par Value
+    'TEA':['Common',0,' ',100],
+    'POP':['Common',8,' ',100],
+    'ALE':['Common',23,' ',60],
+    'GIN':['Preferred',8,2/100,100],
+    'JOE':['Common',13,' ',250],
+}
+
